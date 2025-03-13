c684c077eb44aa2fabc965018ef0d0ee4a0b817c
Johndenver Contaoi <jdenvercontaoi@gmail.com>
2025-03-13 09:53:04 +0800

    resolve cors issue

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: ['http://127.0.0.1:9292','https://italian-corners.myshopify.com'],
+    credentials: true
+}));
 
 // Shopify credentials
 const SHOPIFY_STORE = process.env.SHOPIFY_STORE;
