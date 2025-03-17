f626ecf0c4594807673b81595ceb05a67df5922f
Johndenver Contaoi <jdenvercontaoi@gmail.com>
2025-03-17 19:42:08 +0800

    new changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-    origin: ['http://127.0.0.1:9292','https://italian-corners.myshopify.com'],
+    origin: ['http://127.0.0.1:9292','https://italian-corners.myshopify.com','http://localhost:9292'],
     credentials: true
 }));
 
